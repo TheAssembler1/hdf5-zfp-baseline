c76e374567f3ae3d192f4651a8a3296ef87f75df
Noah Lane Lewis <nlewi26@login19.chn.perlmutter.nersc.gov>
2025-09-17 06:53:21 -0700

    new results

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ set -u
 
 LAUNCHER="srun"
 
-echo "Using $LAUNCHER with $NUM_NODES nodes"
+echo "Using $LAUNCHER with $TOTAL_NODES total nodes"
 
 # Move into build dir
 cd ./build
@@ -26,31 +26,40 @@ function clean_between_workload_files() {
 
 # 1 Argument the path to the workload to runf
 # 2 Argument is just name of workloads for logging
+# 3 Argument is true or false indicating whether PDC servers should be launched/closed
 function run_benchmark() {
 	# Run the benchmark on 
 	for ((i=1; i<=$TOTAL_TASKS; i*=2)); do
 		# Setup names of log files
-		CLIENT_LOG=pdc_client_${i}_$2.log
-		CLIENT_CLOSE_LOG=pdc_client_close_${i}_$2.log
-		SERVER_LOG=pdc_server_${i}_$2.log
-		CLIENT_LOG_ERR=pdc_client_${i}_$2.err
-		CLIENT_CLOSE_LOG_ERR=pdc_client_close_${i}_$2.err
-		SERVER_LOG_ERR=pdc_server_${i}_$2.err
+		CLIENT_LOG=client_${i}_$2.log
+		CLIENT_CLOSE_LOG=client_close_${i}_$2.log
+		SERVER_LOG=server_${i}_$2.log
+		CLIENT_LOG_ERR=client_${i}_$2.err
+		CLIENT_CLOSE_LOG_ERR=client_close_${i}_$2.err
+		SERVER_LOG_ERR=server_${i}_$2.err
 
 		# Print log info
 		echo "CLIENT_LOG=$CLIENT_LOG, SERVER_LOG=$SERVER_LOG, CLIENT_CLOSE_LOG=$CLIENT_CLOSE_LOG"
 		echo "CLIENT_LOG_ERR=$CLIENT_LOG_ERR, SERVER_LOG_ERR=$SERVER_LOG_ERR, CLIENT_CLOSE_LOG_ERR=$CLIENT_CLOSE_LOG_ERR"
 
-		echo "Running benchmark $1 with ${i} ranks"
-		# Launch the pdc server(s)
-		echo "Starting servers"
-		srun --nodes=$NUM_NODES \
-			 --ntasks-per-node=1 \
-			 --error="$SERVER_LOG_ERR" \
-			 --output="$SERVER_LOG" \
-			 pdc_server &
-		echo "Sleeping after server start"
-		sleep 1
+		NUM_NODES=$(( (i + 31) / 32 ))
+		if [ $NUM_NODES -gt $TOTAL_NODES ]; then
+			NUM_NODES=$TOTAL_NODES
+		fi
+
+		echo "Running benchmark $1 with ${i} ranks on $NUM_NODES nodes"
+
+		# Launch the PDC server(s)
+		if [ "$3" = "true" ]; then
+			echo "Starting PDC servers"
+			srun --nodes=$NUM_NODES \
+				--ntasks-per-node=1 \
+				--error="$SERVER_LOG_ERR" \
+				--output="$SERVER_LOG" \
+				pdc_server &
+			echo "Sleeping after server start"
+			sleep 1
+		fi
 
 		echo "Starting benchmark"
 		srun --ntasks=${i} \
@@ -59,19 +68,21 @@ function run_benchmark() {
 			./zfp_baseline $1
 
 		# Launch the pdc server(s)
-		echo "Closing servers"
-		srun --nodes=$NUM_NODES \
-			 --ntasks-per-node=1 \
-			 --error="$CLIENT_CLOSE_LOG_ERR"\
-			--output="$CLIENT_CLOSE_LOG" \
-			close_server
+		if [ "$3" = "true" ]; then
+			echo "Closing PDC servers"
+			srun --nodes=$NUM_NODES \
+				--ntasks-per-node=1 \
+				--error="$CLIENT_CLOSE_LOG_ERR"\
+				--output="$CLIENT_CLOSE_LOG" \
+				close_server
+		fi
 	done
 
 	clean_between_workload_files
 }
 
 clean_old_files
-run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/pdc_raw.json" "pdc_raw"
-run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/pdc_zfp.json" "pdc_zfp"
-run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/hdf5_raw.json" "hdf5_raw"
-run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/hdf5_zfp.json" "hdf5_zfp"
+run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/pdc_raw.json" "pdc_raw" true
+run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/pdc_zfp.json" "pdc_zfp" true
+run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/hdf5_raw.json" "hdf5_raw" false
+run_benchmark "/pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/workloads/hdf5_zfp.json" "hdf5_zfp" false
--- run_perlmutter.sh
+++ run_perlmutter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --nodes=32
+#SBATCH --nodes=256
 #SBATCH --ntasks-per-node=33
 #SBATCH --cpus-per-task=1
 #SBATCH --account=m2621
@@ -10,11 +10,11 @@
 set -x
 set -u
 
-export NUM_NODES=32
+export TOTAL_NODES=256
 export TASKS_PER_NODE=32
-export TOTAL_TASKS=$((NUM_NODES * TASKS_PER_NODE))
+export TOTAL_TASKS=$((TOTAL_NODES * TASKS_PER_NODE))
 
-echo "NUM_NODES = $NUM_NODES"
+echo "TOTAL_NODES = $TOTAL_NODES"
 echo "TASKS_PER_NODE = $TASKS_PER_NODE"
 echo "TOTAL_TASKS = $TOTAL_TASKS"
 
