14a068e06ac651e01cbe36ffd1c5ae5688e9c1b6
Noah Lane Lewis <nlewi26@login05.chn.perlmutter.nersc.gov>
2025-10-03 09:56:27 -0700

    new res

---
+++ new_res/256_cache_no_logs_randomized/build/CMakeFiles/zfp_baseline.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zfp_baseline.dir/src/common/common.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/common/common.c.o.d"
+  "CMakeFiles/zfp_baseline.dir/src/common/config.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/common/config.c.o.d"
+  "CMakeFiles/zfp_baseline.dir/src/common/util.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/common/util.c.o.d"
+  "CMakeFiles/zfp_baseline.dir/src/exec_io_impl.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/exec_io_impl.c.o.d"
+  "CMakeFiles/zfp_baseline.dir/src/hdf5_impl/hdf5_io_impl.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/hdf5_impl/hdf5_io_impl.c.o.d"
+  "CMakeFiles/zfp_baseline.dir/src/main.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/main.c.o.d"
+  "CMakeFiles/zfp_baseline.dir/src/pdc_impl/pdc_io_impl.c.o"
+  "CMakeFiles/zfp_baseline.dir/src/pdc_impl/pdc_io_impl.c.o.d"
+  "zfp_baseline"
+  "zfp_baseline.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zfp_baseline.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ new_res/256_cache_no_logs_randomized/build/CMakeFiles/zfp_baseline.dir/src/exec_io_impl.c.o.d
@@ -0,0 +1,50 @@
+CMakeFiles/zfp_baseline.dir/src/exec_io_impl.c.o: \
+ /pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/src/exec_io_impl.c \
+ /usr/include/stdc-predef.h \
+ /usr/lib64/gcc/x86_64-suse-linux/13/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/sys/cdefs.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/timesize.h \
+ /usr/include/bits/typesizes.h /usr/include/bits/time64.h \
+ /usr/include/bits/wchar.h /usr/include/bits/stdint-intn.h \
+ /usr/include/bits/stdint-uintn.h \
+ /opt/cray/pe/mpich/8.1.30/ofi/gnu/12.3/include/mpi.h \
+ /opt/cray/pe/mpich/8.1.30/ofi/gnu/12.3/include/cray_version.h \
+ /opt/cray/pe/mpich/8.1.30/ofi/gnu/12.3/include/mpio.h \
+ /opt/cray/pe/mpich/8.1.30/ofi/gnu/12.3/include/mpi.h \
+ /usr/lib64/gcc/x86_64-suse-linux/13/include/stdbool.h \
+ /usr/include/stdio.h \
+ /usr/lib64/gcc/x86_64-suse-linux/13/include/stddef.h \
+ /usr/lib64/gcc/x86_64-suse-linux/13/include/stdarg.h \
+ /usr/include/bits/types/__fpos_t.h /usr/include/bits/types/__mbstate_t.h \
+ /usr/include/bits/types/__fpos64_t.h /usr/include/bits/types/__FILE.h \
+ /usr/include/bits/types/FILE.h /usr/include/bits/types/struct_FILE.h \
+ /usr/include/bits/types/cookie_io_functions_t.h \
+ /usr/include/bits/stdio_lim.h /usr/include/bits/sys_errlist.h \
+ /usr/include/bits/stdio.h /usr/include/stdlib.h \
+ /usr/include/bits/waitflags.h /usr/include/bits/waitstatus.h \
+ /usr/lib64/gcc/x86_64-suse-linux/13/include-fixed/bits/floatn.h \
+ /usr/include/bits/types/locale_t.h /usr/include/bits/types/__locale_t.h \
+ /usr/include/sys/types.h /usr/include/bits/types/clock_t.h \
+ /usr/include/bits/types/clockid_t.h /usr/include/bits/types/time_t.h \
+ /usr/include/bits/types/timer_t.h /usr/include/endian.h \
+ /usr/include/bits/endian.h /usr/include/bits/endianness.h \
+ /usr/include/bits/byteswap.h /usr/include/bits/uintn-identity.h \
+ /usr/include/sys/select.h /usr/include/bits/select.h \
+ /usr/include/bits/types/sigset_t.h /usr/include/bits/types/__sigset_t.h \
+ /usr/include/bits/types/struct_timeval.h \
+ /usr/include/bits/types/struct_timespec.h \
+ /usr/include/bits/pthreadtypes.h /usr/include/bits/thread-shared-types.h \
+ /usr/include/bits/pthreadtypes-arch.h /usr/include/bits/struct_mutex.h \
+ /usr/include/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/bits/stdlib-bsearch.h /usr/include/bits/stdlib-float.h \
+ /usr/include/time.h /usr/include/bits/time.h /usr/include/bits/timex.h \
+ /usr/include/bits/types/struct_tm.h \
+ /usr/include/bits/types/struct_itimerspec.h \
+ /pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/src/exec_io_impl.h \
+ /pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/src/common/common.h \
+ /pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/src/common/log.h \
+ /usr/include/assert.h \
+ /pscratch/sd/n/nlewi26/src/hdf5-zfp-baseline/src/common/config.h
